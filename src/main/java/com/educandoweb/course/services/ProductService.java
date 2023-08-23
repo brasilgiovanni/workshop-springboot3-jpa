631c41cd4ea2d679db64c369bed1595778658101
Giovanni Brasil <giovannisbrasil@gmail.com>
2023-08-23 15:39:51 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.repositories.ProductRepository;
+
+@Service // indica que é uma clsse de Serviço, existe ainda o Repository e tambem o Component que é um modo geral
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
